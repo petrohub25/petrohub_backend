788fd42ad3248f8ac25fb9e4cfc97af0eed6348a
Jerry <gs.amezcua@comunidad.unam.mx>
2025-04-04 20:24:32 -0600

    [MOD] modified repositories and added the specific annotation

--- src/main/java/org/unam/petrohub_project/model/documentos/DocumentoRepository.java
+++ src/main/java/org/unam/petrohub_project/model/documentos/DocumentoRepository.java
@@ -5,7 +5,9 @@ import java.util.UUID;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DocumentoRepository extends JpaRepository<Documento, UUID>, JpaSpecificationExecutor<Documento> {
     boolean existsByTitulo(String titulo);
 
